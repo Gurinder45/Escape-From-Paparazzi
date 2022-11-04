783e3fdb7b0ca2b08fdfc962aff80cfa5c2ab091
hma74 <hma74@sfu.ca>
2022-11-04 15:57:09 -0700

    made changes in the map class

--- src/main/java/ui/PausedPanel.java
+++ src/main/java/ui/PausedPanel.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -23,7 +24,12 @@ public class PausedPanel extends JPanel {
 		loadBackground();
 		this.resumeBtn = new JButton("RESUME");
 		this.add(resumeBtn);
-		resumeBtn.setBounds((gFrame.screenWidth / 2) - 40, (gFrame.screenHeight / 2) - 15, 80, 30);
+		//resumeBtn.setBounds((gFrame.screenWidth / 2) - 40, (gFrame.screenHeight / 2) - 15, 80, 30);
+		resumeBtn.setBounds(710, 450, 250, 100);
+		resumeBtn.setBackground(new Color(225, 225, 0));
+		resumeBtn.setForeground(Color.BLACK);
+		resumeBtn.setFocusPainted(false);
+		resumeBtn.setFont(new Font("Serif", Font.BOLD, 40));
 		addActListeners();
 
 	}
@@ -56,8 +62,10 @@ public class PausedPanel extends JPanel {
 		int x = (gFrame.screenWidth / 2) - (2 * gFrame.cellSize);
 		int y = gFrame.screenHeight / 4;
 
-		g2d.setColor(Color.white);
-		g2d.drawString(text, x, y);
+		g2d.setFont(new Font("Serif", Font.BOLD, 65));
+		g2d.setColor(Color.black);
+		g2d.drawString(text, 730, 370);
+		//g2d.drawString(text, x, y);
 	}
 }
 
